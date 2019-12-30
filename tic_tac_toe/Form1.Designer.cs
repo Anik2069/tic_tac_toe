4528d0807ef2fa3bbb758828d8f863fde844c0f6
anikdiu <anik35-2069@diu.edu.bd>
2019-12-31 02:59:51 +0600

    First commit

---
+++ tic_tac_toe/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.p1 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.ss = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.Location = new System.Drawing.Point(12, 23);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(92, 75);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A01
+            // 
+            this.A01.Location = new System.Drawing.Point(110, 23);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(92, 75);
+            this.A01.TabIndex = 1;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A02
+            // 
+            this.A02.Location = new System.Drawing.Point(208, 23);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(92, 75);
+            this.A02.TabIndex = 2;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(12, 104);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(92, 75);
+            this.A10.TabIndex = 3;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A11
+            // 
+            this.A11.Location = new System.Drawing.Point(110, 104);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(92, 75);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A12
+            // 
+            this.A12.Location = new System.Drawing.Point(208, 104);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(92, 75);
+            this.A12.TabIndex = 5;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(12, 185);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(92, 75);
+            this.A20.TabIndex = 6;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(110, 185);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(92, 75);
+            this.A21.TabIndex = 7;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(208, 185);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(92, 75);
+            this.A22.TabIndex = 8;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.A00_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(47, 280);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "New Game";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(155, 280);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Reset";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Player1 Score :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Player2 Score :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // p1
+            // 
+            this.p1.AutoSize = true;
+            this.p1.Location = new System.Drawing.Point(338, 85);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(0, 13);
+            this.p1.TabIndex = 14;
+            // 
+            // p2
+            // 
+            this.p2.AutoSize = true;
+            this.p2.Location = new System.Drawing.Point(338, 166);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(0, 13);
+            this.p2.TabIndex = 15;
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Location = new System.Drawing.Point(326, 201);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(65, 13);
+            this.l3.TabIndex = 16;
+            this.l3.Text = "MatchDraw:";
+            // 
+            // ss
+            // 
+            this.ss.AutoSize = true;
+            this.ss.Location = new System.Drawing.Point(338, 227);
+            this.ss.Name = "ss";
+            this.ss.Size = new System.Drawing.Size(0, 13);
+            this.ss.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 320);
+            this.Controls.Add(this.ss);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label ss;
+    }
+}
+
---
+++ tic_tac_toe/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int player = 2;
+        public int turns = 0;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+
+        public bool onturn = true;
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void A00_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+            {
+                /*  if (player % 2 == 0)
+                  {
+                      btn.Text = "X";
+                      player++;
+                      turns++;
+                  }
+                  else
+                  {
+                      btn.Text = "O";
+                      player++;
+                      turns++;
+
+                  }*/
+                if (onturn == true)
+                {
+                    btn.Text = "X";
+                }
+                else
+                {
+                    btn.Text = "0";
+
+                }
+                turns++;
+                onturn = !onturn;
+            }
+                if (checkdraw()==true && checkwinner() == false) {
+                    MessageBox.Show("Match Draw");
+                     sd++;
+                    newgame();
+                }
+                if (checkwinner() == true)
+                {
+                    if (btn.Text == "X")
+                    {
+                        s1++;
+                        MessageBox.Show("Player 1 Win!!!!!");
+                        newgame();
+
+                    }
+                    else
+                    {
+                        s2++;
+                        MessageBox.Show("Player 2 Win!!!!!");
+                        newgame();
+
+                    }
+                }
+            if (onturn==false)
+            {
+                CPU().PerformClick();
+            }
+
+            
+          
+        }
+
+        public Button CPU()
+        {
+            Button b = null;
+            b = CPUTryWinorDefend("0");
+            if (b!=null)
+            {
+                return b;
+            }
+            else
+            {
+                b = CPUTryWinorDefend("X");
+                if (b != null)
+                {
+                    return b;
+                }
+                else
+                {
+                    return CPUMoveRandom();
+                }
+            }
+        } 
+
+        public Button CPUTryWinorDefend(string s)
+        {
+            Button b = null;
+
+            if (A00.Text == A01.Text && A00.Text == s && A02.Text == "")
+            {
+                return A02;
+            }
+            else if (A00.Text == A02.Text && A00.Text == s && A01.Text == "")
+
+            {
+                return A01;
+            }
+            else if (A01.Text == A02.Text && A01.Text == A22.Text && A00.Text == "")
+            {
+                return A00;
+            }
+
+
+            else if (A10.Text == A12.Text && A10.Text == s && A11.Text == "")
+            {
+                return A11;
+            }
+            else if (A10.Text == A11.Text && A10.Text ==s && A02.Text == "")
+            {
+                return A02;
+            }
+            else if (A12.Text == A11.Text && A11.Text == s && A10.Text == "")
+            {
+                return A10;
+            }
+
+
+            else if (A20.Text == A22.Text && A20.Text == s && A21.Text == "")
+            {
+                return A21;
+            }
+
+            else if (A20.Text == A21.Text && A20.Text == s && A22.Text == "")
+            {
+                return A22;
+            }
+
+
+            else if (A22.Text == A21.Text && A22.Text == s && A20.Text == "")
+            {
+                return A20;
+            }
+
+
+            else if (A00.Text == A10.Text && A00.Text == s && A20.Text == "")
+            {
+                return A20;
+            }
+
+
+            else if (A00.Text == A20.Text && A00.Text == s && A10.Text == "")
+            {
+                return A10;
+            }
+
+
+            else if (A20.Text == A10.Text && A10.Text == s && A00.Text == "")
+            {
+                return A00;
+            }
+
+
+            else if (A21.Text == A11.Text && A11.Text == s && A01.Text == "")
+            {
+                return A01;
+            }
+
+
+            else if (A21.Text == A01.Text && A01.Text == s && A11.Text == "")
+            {
+                return A11;
+            }
+
+
+            else if (A01.Text == A11.Text && A11.Text == s && A21.Text == "")
+            {
+                return A21;
+            }
+
+
+
+            else if (A22.Text == A02.Text && A02.Text == s && A12.Text == "")
+            {
+                return A12;
+            }
+
+
+            else if (A02.Text == A12.Text && A01.Text == s && A22.Text == "")
+            {
+                return A22;
+            }
+
+
+            else if (A00.Text == A11.Text && A11.Text == s && A22.Text == "")
+            {
+                return A22;
+            }
+
+
+            else if (A22.Text == A00.Text && A22.Text == s && A11.Text == "")
+            {
+                return A11;
+            }
+            //done
+            else if (A22.Text == A11.Text && A11.Text == s && A00.Text == "")
+            {
+                return A00;
+            }
+
+
+            else if (A20.Text == A11.Text && A11.Text == s && A02.Text == "")
+            {
+                return A02;
+            }
+
+
+            else if (A20.Text == A02.Text && A02.Text == s && A11.Text == "")
+            {
+                return A11;
+            }
+
+
+
+            else if (A02.Text == A11.Text && A11.Text == s && A20.Text == "")
+            {
+                return A20;
+            }
+
+
+            else
+            {
+                return null;
+            }
+
+        }
+        public Button CPUMoveRandom()
+        {
+            Button b = null;
+            foreach (Control c in Controls)
+            {
+                b = c as Button;
+                if (b != null)
+                {
+                    if (b.Text == "")
+                    {
+                        return b;
+                    }
+                }
+            }
+            return null;
+        }
+        void newgame()
+        {
+            player = 2;
+            turns = 0;
+            A00.Text = A01.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "";
+            p1.Text = "" + s1;
+            p2.Text = "" + s2;
+            ss.Text = "" + sd;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            newgame();
+        }
+        bool checkdraw()
+        {
+            if (turns == 9)
+                return true;
+            else
+                return false;
+        }
+        bool checkwinner()
+        {
+            if (A00.Text == A01.Text &&  A01.Text == A02.Text && A00.Text!="")
+            {
+                return true;
+            }
+            else if (A10.Text == A11.Text && A11.Text == A12.Text && A10.Text != "")
+            {
+                return true;
+            }
+            else if (A20.Text == A21.Text && A21.Text == A22.Text && A20.Text != "")
+            {
+                return true;
+            }
+
+
+             if (A00.Text == A10.Text &&  A10.Text == A20.Text && A00.Text!="")
+            {
+                return true;
+            }
+            else if (A11.Text == A01.Text && A11.Text == A21.Text && A01.Text != "")
+            {
+                return true;
+            }
+            else if (A02.Text == A12.Text && A12.Text == A22.Text && A02.Text != "")
+            {
+                return true;
+            }
+
+
+            if (A00.Text == A11.Text && A11.Text == A22.Text && A00.Text != "")
+            {
+                return true;
+            }
+          
+            else if (A02.Text == A11.Text && A11.Text == A20.Text && A02.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p1.Text =""+ s1;
+            p2.Text = "" + s2;
+            ss.Text = "" + sd;
+             
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            s1 = s2 = sd = 0;
+            newgame();
+        }
+    }
+}
